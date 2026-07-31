25ef90af0079c3f4a432a9baf3d5786d03e6679c
Rudra Jena <rnj.rudra3@gmail.com>
2026-07-31 08:09:18 +0530

    commit on 2026-07-31 08:09:18

--- src/src/ThreeSum.java
+++ src/src/ThreeSum.java
@@ -1,2 +1,43 @@
+import java.util.*;
+
 public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) { // Optimization: stop at n-2
+            // Skip duplicate values for 'i'
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+
+                    // Skip duplicates for 'left' and 'right'
+                    while (left < right && arr[left] == arr[left + 1]) left++;
+                    while (left < right && arr[right] == arr[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result; // Return after checking all elements
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-4, -1, -1, 0, 1, 2};
+        System.out.println(threeSum(arr));
+    }
 }
